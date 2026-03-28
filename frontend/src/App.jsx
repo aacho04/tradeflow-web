e64697a26c9e2c9af9968826394178a7a9b6889c
aacho04 <aaditinarayan14@gmail.com>
2026-03-28 20:06:27 +0530

    Home-page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css'
+import Home from "./pages/Home/Home";
 
 function App() {
  return (
-   <h1>hello</h1>
+   <>
+   <Home/>
+   </>
   )
 }
 
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,23 @@
 import react from 'react';
-import Pricing from '../Pricing/Pricing';
+import OpenAccount from "../../components/OpenAccount/OpenAccount";
+import Hero from "./Hero";
+import Awards from "./Awards";
+import Stats from "./Stats";
+import Pricing from "../Pricing/Pricing";
+import Education from "./Education";
+import Footer from "../../components/Footer/Footer";
+import Navbar from "../../components/Navbar/Navbar";    
 function Home(){
     return(
         <>
+        <Navbar/>
         <Hero/>
         <Awards/>
         <Stats/>
         <Pricing/>
         <Education/>
+        <OpenAccount/>
+        <Footer/>
         </>
     );
 
