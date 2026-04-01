edfd4275025ba05e2903378bd3d1469f160ccb93
aacho04 <aaditinarayan14@gmail.com>
2026-04-01 08:18:19 +0530

    feat: add Hero section component for landing page

--- frontend/src/pages/Home/Hero.jsx
+++ frontend/src/pages/Home/Hero.jsx
@@ -1,8 +1,17 @@
 import react from 'react';
 function Hero(){
-    return(<h1>Hero
-        
-</h1>);
+    return(
+        <div className="container">
+            <div className="row">
+            <img src="../media/trading.png" alt="trading" className="img-fluid"/>
+            <h1>Invest in Everything</h1>
+            <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+            <button>Signup Now</button>
+            </div>
+            
+
+        </div>
+    );
 
 }
 export default Hero;
