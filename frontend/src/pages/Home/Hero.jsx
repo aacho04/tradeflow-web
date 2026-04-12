0becc2c71f44f7a9f62f190f5b050468eba902a0
aacho04 <aaditinarayan14@gmail.com>
2026-04-12 08:42:35 +0530

    Hero section with trading img

--- frontend/src/pages/Home/Hero.jsx
+++ frontend/src/pages/Home/Hero.jsx
@@ -1,12 +1,12 @@
 import react from 'react';
 function Hero(){
     return(
-        <div className="container">
-            <div className="row">
-            <img src="../media/trading.png" alt="trading" className="img-fluid"/>
-            <h1>Invest in Everything</h1>
-            <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-            <button>Signup Now</button>
+        <div className="container p-5">
+            <div className="row text-center" >
+            <img src="../media/trading.png" alt="trading" className="img-fluid mb-5"/>
+            <h1 className="mt-4 fs-2 ">Invest in Everything</h1>
+            <p >Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+            <button className="p-2 btn btn-primary fs-6"style={{width:"15%",margin:"0 auto"}}>Signup Now</button>
             </div>
             
 
