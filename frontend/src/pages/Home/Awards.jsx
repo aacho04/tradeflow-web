2cae6b3c4a5f11161a2424bafbd883f080d15e9f
aacho04 <aaditinarayan14@gmail.com>
2026-04-12 09:34:50 +0530

    Herosection  Bootstraping

--- frontend/src/pages/Home/Awards.jsx
+++ frontend/src/pages/Home/Awards.jsx
@@ -1,6 +1,18 @@
 import react from 'react';
 function Awards(){
-    return(<h1>Awards</h1>);
+    return(
+    <div className="container mt-5">
+        <div className="row">
+            <div className="col-12 col-md-6">
+                <img src="../media/largest-broker.svg" alt="Broker image" />
+
+            </div>
+            <div className="col-12 col-md-6">
+
+            </div>
+        </div>
+    </div>
+    );
 
 }
 export default Awards;
--- frontend/src/pages/Home/Hero.jsx
+++ frontend/src/pages/Home/Hero.jsx
@@ -1,12 +1,12 @@
 import react from 'react';
 function Hero(){
     return(
-        <div className="container p-5">
+        <div className="container p-4 mb-5">
             <div className="row text-center" >
-            <img src="../media/trading.png" alt="trading" className="img-fluid mb-5"/>
-            <h1 className="mt-4 fs-2 ">Invest in Everything</h1>
-            <p >Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-            <button className="p-2 btn btn-primary fs-6"style={{width:"15%",margin:"0 auto"}}>Signup Now</button>
+            <img className="img-fluid d-block mx-auto" style={{width:"60%"}} src="../media/landing.svg" alt="trading"/>
+            <h1 className="mt-5 fs-4 mb-2 text-dark">Invest in Everything</h1>
+            <p className='fs-5 text-muted'>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+            <button className="p-2 btn btn-primary fs-6 mb-5 mt-4"style={{width:"15%",margin:"0 auto"}}>Signup Now</button>
             </div>
             
 
